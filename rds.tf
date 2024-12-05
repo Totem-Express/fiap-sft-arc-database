0e0180a1b18204903c09cec1823537e7adfa8b42
João Henrique Luciano <jao.luciano8@gmail.com>
2024-12-05 20:18:31 -0300

    Passando versão do BD como variável do terraform

--- rds.tf
+++ rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = var.db_allocated_storage
   instance_class   = var.db_instance_class
   engine             = "mysql"
-  engine_version      = "8.0"  # Specify desired MySQL version
+  engine_version      = var.db_version  # Specify desired MySQL version
   # name                = var.db_name
   username            = var.db_username
   password            = var.db_password
